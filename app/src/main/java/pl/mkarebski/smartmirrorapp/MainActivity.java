25834347c0296dbcad67923f3236b724eac958fe
mikolaj <mikolaj.karebski@gmail.com>
2017-01-16 21:53:41 +0100

    Replace multiply alarms with one with hourly interval

--- app/src/main/java/pl/mkarebski/smartmirrorapp/MainActivity.java
+++ app/src/main/java/pl/mkarebski/smartmirrorapp/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.johnhiott.darkskyandroidlib.ForecastApi;
 
@@ -20,7 +19,7 @@ import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
-    private static final String FORECAST_IO_API_KEY = "API_KEY";
+    private static final String FORECAST_IO_API_KEY = "YOUR_API_KEY";
     public static final String MyPREFERENCES = "MyPrefs";
     private TextView temperature;
     private ImageView weatherIcon;
@@ -44,20 +43,24 @@ public class MainActivity extends Activity {
 
         SharedPreferences sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
+        clearOldSharedPreferences(sharedpreferences);
 
         sharedpreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                Log.d("SharedprefsListener", "changed");
+
                 String temp = sharedPreferences.getString("temp", "");
                 String icon = sharedPreferences.getString("icon", "");
-
-                Log.d("SharedprefsListener", "temp");
-                Log.d("SharedprefsListener", "icon");
 
                 temperature.setText(temp);
                 weatherIcon.setImageResource(getResourceFrom(icon));
             }
         });
+    }
+
+    private void clearOldSharedPreferences(SharedPreferences sharedpreferences) {
+        sharedpreferences.edit().clear().apply();
     }
 
     private PendingIntent initPendingIntentWithRequestCodeEqualTo(int i) {
@@ -73,30 +76,10 @@ public class MainActivity extends Activity {
     private void setUpAlarm() {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        Calendar morning = getInitiatedCalendar(7, 30);
-        Calendar noon = getInitiatedCalendar(12, 30);
-        Calendar afternoon = getInitiatedCalendar(16, 30);
-        Calendar evening = getInitiatedCalendar(20, 30);
-
-        PendingIntent pendingIntent1 = initPendingIntentWithRequestCodeEqualTo(1);
-        PendingIntent pendingIntent2 = initPendingIntentWithRequestCodeEqualTo(2);
-        PendingIntent pendingIntent3 = initPendingIntentWithRequestCodeEqualTo(3);
-        PendingIntent pendingIntent4 = initPendingIntentWithRequestCodeEqualTo(4);
-
-        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, morning.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent1);
-        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, noon.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent2);
-        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, afternoon.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent3);
-        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, evening.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent4);
-
-        Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show();
-    }
-
-    private Calendar getInitiatedCalendar(int h, int m) {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, h);
-        calendar.set(Calendar.MINUTE, m);
-        return calendar;
+
+        PendingIntent pendingIntent = initPendingIntentWithRequestCodeEqualTo(1);
+
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTime().getTime() + 5000, AlarmManager.INTERVAL_HOUR, pendingIntent);
     }
-
-
 }
