97bf6295e587598f7a6746efe18dab2dfd4fa2f4
mikolaj <mikolaj.karebski@gmail.com>
2017-01-18 19:19:23 +0100

    Changed weather info provider from Forecast.io to Weather.com

--- app/src/main/java/pl/mkarebski/smartmirrorapp/ForecastDownloader.java
+++ app/src/main/java/pl/mkarebski/smartmirrorapp/ForecastDownloader.java
@@ -6,75 +6,103 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 
-import com.johnhiott.darkskyandroidlib.RequestBuilder;
-import com.johnhiott.darkskyandroidlib.models.Request;
-import com.johnhiott.darkskyandroidlib.models.WeatherResponse;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+import cz.msebera.android.httpclient.Header;
 
 import static android.content.Context.MODE_PRIVATE;
 import static java.lang.String.format;
+import static java.lang.String.valueOf;
 import static pl.mkarebski.smartmirrorapp.MainActivity.MyPREFERENCES;
 
 public class ForecastDownloader extends BroadcastReceiver {
 
-    private static final String LAT = "50.8660773";
-    private static final String LNG = "20.6285677";
-    private static final String TAG = "WeatherService";
     private static final String CELSIUS = "\u2103";
+    private static final String API_KEY = "YOUR_API_KEY"; //weather.com
+    private static final String URL = "http://api.wunderground.com/api/" + API_KEY + "/conditions/q/PL/Kielce.json";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         final Context con = context;
 
-        RequestBuilder weather = new RequestBuilder();
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, new AsyncHttpResponseHandler() {
 
-        weather.getWeather(request(), new Callback<WeatherResponse>() {
             @Override
-            public void success(WeatherResponse weatherResponse, Response response) {
-                double temp = weatherResponse.getCurrently().getApparentTemperature();
-                Log.d(TAG, "Temp: " + temp);
+            public void onStart() {
+                // called before request is started
+            }
 
-                String icon = weatherResponse.getCurrently().getIcon();
-                Log.d(TAG, "Icon: " + icon);
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+
+                String responseBody = new String(response);
+
+                JSONObject root = null;
+                try {
+                    root = new JSONObject(responseBody);
+                } catch (JSONException e) {
+                    throw new IllegalStateException(e);
+                }
+
+                JSONObject currentObservation = null;
+                try {
+                    currentObservation = root.getJSONObject("current_observation");
+                } catch (JSONException e) {
+                    throw new IllegalStateException(e);
+                }
+
+                String temp = null;
+                try {
+                    temp = currentObservation.getString("temp_c");
+                } catch (JSONException e) {
+                    throw new IllegalStateException(e);
+                }
+
+                String icon = null;
+                try {
+                    icon = currentObservation.getString("icon");
+                } catch (JSONException e) {
+                    throw new IllegalStateException(e);
+                }
 
                 updateSharedPreferences(temp, icon, con);
             }
 
             @Override
-            public void failure(RetrofitError retrofitError) {
-                Log.d(TAG, "Error while calling: " + retrofitError.getUrl());
-                Log.d(TAG, "Message: " + retrofitError.getMessage());
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                Log.d("WeatherService", valueOf(statusCode));
+                Log.d("WeatherService", valueOf(e.getMessage()));
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
             }
         });
+
+
     }
 
-    private void updateSharedPreferences(double temp, String icon, Context con) {
-        Log.d("FD_SP1", String.valueOf(temp));
+    private void updateSharedPreferences(String temp, String icon, Context con) {
+        Log.d("FD_SP1", temp);
         Log.d("FD_SP1", icon);
 
         SharedPreferences sharedPreferences = con.getSharedPreferences(MyPREFERENCES, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("temp", format("%.0f" + CELSIUS, Math.ceil(temp)));
+        editor.putString("temp", format("%.0f" + CELSIUS, Math.ceil(new Double(temp))));
         editor.putString("icon", icon);
         editor.apply();
 
         Log.d("FD_SP2", sharedPreferences.getString("temp", ""));
         Log.d("FD_SP2", sharedPreferences.getString("icon", ""));
-    }
-
-    private Request request() {
-        Request request = new Request();
-        request.setLat(LAT);
-        request.setLng(LNG);
-        request.setUnits(Request.Units.SI);
-        request.setLanguage(Request.Language.POLISH);
-        request.addExcludeBlock(Request.Block.HOURLY);
-        request.addExcludeBlock(Request.Block.MINUTELY);
-        request.addExcludeBlock(Request.Block.DAILY);
-        return request;
     }
 
 }
--- app/src/main/java/pl/mkarebski/smartmirrorapp/MainActivity.java
+++ app/src/main/java/pl/mkarebski/smartmirrorapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -13,22 +14,21 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.johnhiott.darkskyandroidlib.ForecastApi;
-
 import java.util.Calendar;
+
+import static android.app.AlarmManager.INTERVAL_HALF_HOUR;
+import static java.util.Calendar.*;
 
 public class MainActivity extends Activity {
 
-    private static final String FORECAST_IO_API_KEY = "YOUR_API_KEY";
     public static final String MyPREFERENCES = "MyPrefs";
     private TextView temperature;
     private ImageView weatherIcon;
+    private OnSharedPreferenceChangeListener onSharedPreferenceChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ForecastApi.create(FORECAST_IO_API_KEY);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
 
         clearOldSharedPreferences(sharedpreferences);
 
-        sharedpreferences.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+        onSharedPreferenceChangeListener = new OnSharedPreferenceChangeListener() {
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
                 Log.d("SharedprefsListener", "changed");
@@ -56,7 +56,8 @@ public class MainActivity extends Activity {
                 temperature.setText(temp);
                 weatherIcon.setImageResource(getResourceFrom(icon));
             }
-        });
+        };
+        sharedpreferences.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
     }
 
     private void clearOldSharedPreferences(SharedPreferences sharedpreferences) {
@@ -69,17 +70,70 @@ public class MainActivity extends Activity {
     }
 
     private int getResourceFrom(String icon) {
-        String replacedIcon = icon.replaceAll("-", "_");
-        return this.getResources().getIdentifier(replacedIcon, "drawable", this.getPackageName());
+        switch (icon) {
+            case "clear":
+            case "sunny":
+                return getResourceByName("clear_day");
+            case "nt_clear":
+            case "nt_sunny":
+                return getResourceByName("clear_night");
+            case "fog":
+            case "hazy":
+            case "nt_fog":
+            case "nt_hazy":
+                return getResourceByName("fog");
+            case "cloudy":
+            case "nt_cloudy":
+                return getResourceByName("cloudy");
+            case "mostlycloudy":
+            case "mostlysunny":
+            case "partlysunny":
+            case "partlycloudy":
+                return getResourceByName("partly_cloudy_day");
+            case "nt_mostlycloudy":
+            case "nt_mostlysunny":
+            case "nt_partlysunny":
+            case "nt_partlycloudy":
+                return getResourceByName("partly_cloudy_night");
+            case "chanceflurries":
+            case "flurries":
+            case "nt_chanceflurries":
+            case "nt_flurries":
+                return getResourceByName("flurries");
+            case "nt_snow":
+            case "nt_chancesnow":
+            case "snow":
+            case "chancesnow":
+                return getResourceByName("snow");
+            case "chancerain":
+            case "chancesleet":
+            case "sleet":
+            case "rain":
+            case "nt_chancerain":
+            case "nt_chancesleet":
+            case "nt_sleet":
+            case "nt_rain":
+                return getResourceByName("rain");
+            case "chancetstorms":
+            case "tstorms":
+            case "nt_chancetstorms":
+            case "nt_tstorms":
+                return getResourceByName("stormy");
+        }
+        return -1;
+    }
+
+    private int getResourceByName(String name) {
+        return this.getResources().getIdentifier(name, "drawable", this.getPackageName());
     }
 
     private void setUpAlarm() {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        Calendar calendar = Calendar.getInstance();
+        Calendar calendar = getInstance();
 
         PendingIntent pendingIntent = initPendingIntentWithRequestCodeEqualTo(1);
 
-        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTime().getTime() + 5000, AlarmManager.INTERVAL_HOUR, pendingIntent);
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTime().getTime() + 5000, INTERVAL_HALF_HOUR, pendingIntent);
     }
 }
